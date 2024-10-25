e3f3e1c85a9400bd24221b1c7ddf16a26c2240d8
itlearn360 <40198951+itlearn360@users.noreply.github.com>
2024-10-25 18:16:05 +0530

    Update Login.java

--- src/selenium_package/Login.java
+++ src/selenium_package/Login.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Login {
+public class LoginImplicitWait {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
